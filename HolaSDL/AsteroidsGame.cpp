afe96bfce9ec7d89180b59ffcbf039e2e3ddc873
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2019-02-13 15:57:33 +0100

    Initial commit

---
+++ HolaSDL/AsteroidsGame.cpp
@@ -0,0 +1,94 @@
+#include "AsteroidsGame.h"
+#include "Fighter.h"
+
+
+using namespace std;
+
+AsteroidsGame::AsteroidsGame() :
+		SDLGame("Asteroids", _WINDOW_WIDTH_, _WINDOW_HEIGHT_),
+		demoContainer_(this)
+{
+	initGame();
+}
+
+AsteroidsGame::~AsteroidsGame() {
+	closeGame();
+}
+
+void AsteroidsGame::initGame() {
+	BulletsShooter* bs = new BulletsShooter(this);
+	GameObject* fighter = new Fighter(this, bs);
+	fighter->setWidth(75);
+	fighter->setHeight(75);
+	fighter->setPosition(Vector2D(getWindowWidth() / 2, getWindowHeight() / 2));
+	fighter->setVelocity(Vector2D(2, 0));
+
+	actors_.push_back(&demoContainer_);
+	actors_.push_back(fighter);
+}
+
+void AsteroidsGame::closeGame() {
+}
+
+void AsteroidsGame::start() {
+	exit_ = false;
+
+	while (!exit_) {
+		Uint32 startTime = SDL_GetTicks();
+		handleInput(startTime);
+		update(startTime);
+		render(startTime);
+
+		Uint32 frameTime = SDL_GetTicks() - startTime;
+		if (frameTime < 10)
+			SDL_Delay(10 - frameTime);
+	}
+}
+
+void AsteroidsGame::stop() {
+	exit_ = true;
+}
+
+void AsteroidsGame::handleInput(Uint32 time) {
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_KEYDOWN) {
+
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				exit_ = true;
+				break;
+			// Pressing f to toggle fullscreen.
+			case SDLK_f:
+				int flags = SDL_GetWindowFlags(window_);
+				if (flags & SDL_WINDOW_FULLSCREEN) {
+					SDL_SetWindowFullscreen(window_, 0);
+				} else {
+					SDL_SetWindowFullscreen(window_, SDL_WINDOW_FULLSCREEN);
+				}
+				break;
+			}
+		}
+		for (GameObject* o : actors_) {
+			o->handleInput(time, event);
+		}
+	}
+}
+
+void AsteroidsGame::update(Uint32 time) {
+	for (GameObject* o : actors_) {
+		o->update(time);
+	}
+}
+
+void AsteroidsGame::render(Uint32 time) {
+	SDL_SetRenderDrawColor(getRenderer(), COLOR(0x00AAAAFF));
+	SDL_RenderClear(getRenderer());
+
+	for (GameObject* o : actors_) {
+		o->render(time);
+	}
+
+	SDL_RenderPresent(getRenderer());
+}
+
